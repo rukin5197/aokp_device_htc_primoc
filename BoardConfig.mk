c3802439a98f3e77e363be63857fcc2fc1acc9e0
Gannon Macdonald <gannon050197@gmail.com>
2013-01-31 22:54:57 -0600

    Revert "ion"
    This reverts commit 63c4cb9e52d183a3ed4cdd846e8ee8042a98a3b0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,6 @@
 # WARNING: This line must come *before* including the proprietary
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
-
 USE_CAMERA_STUB := true
 
 # inherit from common msm7x30
@@ -59,8 +58,7 @@ ARCH_ARM_HAVE_NEON := true
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
 DYNAMIC_SHARED_LIBV8SO := true
-TARGET_USES_ION := true
-BOARD_HAVE_OLD_ION_API := true
+TARGET_USES_PMEM := true
 
 # Wifi related defines
 WIFI_BAND                        := 802_11_ABG
@@ -103,7 +101,7 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 
-TARGET_KERNEL_SOURCE := kernel/htc/primoc-ion
+TARGET_KERNEL_SOURCE := kernel/htc/primoc
 TARGET_KERNEL_CONFIG := primoc_defconfig
 TARGET_PREBUILT_KERNEL := device/htc/primoc/prebuilt/root/kernel
 BUILD_KERNEL := true
